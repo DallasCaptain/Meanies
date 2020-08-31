1db0af7262ce8ba2c2a739ce2e00da53d747fb16
Chris Thompson <cthompson@codingdojo.com>
2020-08-31 15:19:44 -0500

    i like cookies

---
+++ algos/bubble.js
@@ -0,0 +1,49 @@
+// for(let i=0;i<5;i++){
+
+//     console.log(i)
+//     console.log(i)
+
+// }
+// let condition = true
+// let i = 0
+// let y = 9
+
+// if( i <= y ){
+//     console.log('Yepp there equal')
+// }
+
+let arr = ['asdf',234,{}]
+
+let dict = {'name':  'b"o"b', 'age' : 78, favs: ['red', 'redder','reddest']}
+// console.log(dict.name)
+// console.log(dict['age'])
+//console.log(dict['favs'][0])
+
+//both work
+
+//arr[2]['age'] = 9
+// arr[2].age = 9
+// console.log(arr[2].age)
+
+// let fun = function(stuff){
+//     console.log(stuff)
+//     return('changed'+7)
+// }
+// // function fun(stuff){
+    
+// // }
+
+// console.log(fun('Welcome to my first param','extra param'))
+
+
+
+// while(condition){
+//     console.log(i)
+//     i+=1
+//     if(i === 5)
+//         break
+    
+    
+// }
+
+
