3471026fad5b8c024f2b7683fd8453cc2cf3be43
Chris Thompson <cthompson@codingdojo.com>
2020-09-01 17:00:54 -0500

    WK1D2 demo- POKI API AJAX

--- wk1/ajaxreview.js
+++ wk1/ajaxreview.js
@@ -1,15 +1,39 @@
 function cb (){
     
     function theclickfunction(event){
-        $.get('https://pokeapi.co/api/v2/pokemon/charizard', function(data){
-            console.log(data)
+        $.get('https://pokeapi.co/api/v2/pokemon/', function(data){
+           
+            for(poke of data.results){
+                $.get(poke.url,function(data){
+                    addPokemon({
+                        name:data.name,
+                        img:data.sprites.front_default
+                    })
+                })
+            }
+            
         })
+        
+        
 
-        console.log(event)
+
+     
     } 
     $('#dabutton').click(theclickfunction);
 
-    
+    function addPokemon(obj){
+        
+        let nametd = document.createElement('td');
+        let imgtd = document.createElement('td');
+        let img = document.createElement('img');
+        let trow = document.createElement('tr');
+        img.src = obj.img
+        imgtd.appendChild(img)
+        nametd.innerText = obj.name
+        trow.appendChild(nametd)
+        trow.appendChild(imgtd)
+        $('tbody').append(trow)
+    }
 }
 
 
