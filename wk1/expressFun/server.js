3c98d080ed75a5ac78b6bbbbdc5d38a0f5d6905c
Chris Thompson <cthompson@codingdojo.com>
2020-09-04 16:33:00 -0500

    wk1d5 demo - mongo/mongoose intro

--- wk1/expressFun/server.js
+++ wk1/expressFun/server.js
@@ -2,6 +2,24 @@ const express = require('express')
 const app = express()
 const session = require('express-session')
 const ejs = require('ejs')
+const mongoose = require('mongoose')
+
+    mongoose.connect('mongodb://localhost/meaniesfirstDB', {useNewUrlParser: true})
+
+    const MeanieSchema = new mongoose.Schema({
+        name: {type:String,required:true, minlength: [3, 'NO TYS ALOUD']},
+        progress: Number,
+        email: {type:String,required:true}
+    })
+
+    const Meanie = mongoose.model('Meanie', MeanieSchema);
+
+
+
+
+
+
+
 
     app.use(session({
         secret: 'i solemnly swear i am upto no good',
@@ -16,7 +34,30 @@ const ejs = require('ejs')
     app.set('views', __dirname + '/views')
 
     app.use(express.urlencoded({extended: true}));
-
+    
+    
+    
+    
+    app.get('/meanie/new',(req,res)=>{
+        res.render('newMeanie')
+    })
+    
+    
+    app.post('/meanie',(req,res)=>{
+        newmeanie = new Meanie()
+        newmeanie.name = req.body.name
+        newmeanie.email = req.body.email
+        newmeanie.progress = 0
+        newmeanie.save()
+        .then(data =>{
+            console.log(data);
+            res.redirect('/meanie/new')
+        })
+        .catch(err => {
+            console.log('err',err);
+            res.redirect('/meanie/new')
+        })
+    })
     app.get('/',(req,res)=>{
         res.send('Hello Class')
     })
@@ -31,6 +72,8 @@ const ejs = require('ejs')
     app.get('/myform',(req,res)=>{
         res.render('myform')
     })
+
+
     app.get('/mypost/:name/',(req,res)=>{
         if(!req.session.counter)
         req.session.counter = 0
@@ -45,3 +88,5 @@ const ejs = require('ejs')
         res.render('myform')
     })
 app.listen(8080, ()=>{console.log('listening on port 8080')})
+
+
